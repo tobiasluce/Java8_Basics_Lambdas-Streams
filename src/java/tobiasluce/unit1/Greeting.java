859493b414ece3e889b0a03167dbacfb6a6df927
Tobias Luce <Tobias.Luce@msg-david.de>
2017-05-09 15:54:51 +0200

    Add files via upload

---
+++ src/java/tobiasluce/unit1/Greeting.java
@@ -0,0 +1,10 @@
+package java.tobiasluce.unit1;
+
+@FunctionalInterface
+public interface Greeting {
+	public void perform();
+	
+	
+
+
+}
---
+++ src/java/tobiasluce/unit1/HelloWorldGreeting.java
@@ -0,0 +1,11 @@
+package java.tobiasluce.unit1;
+
+public class HelloWorldGreeting implements Greeting {
+
+	@Override
+	public void perform() {
+		System.out.print("Hello world!");
+
+	}
+
+}
